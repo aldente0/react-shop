cb2e4e95c31dd59444b36a436a1e10a4b39ea3e7
al_dente0 <al_dente0@whatever.com>
2022-02-09 10:50:41 +0300

    add links on github repository

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export function Footer() {
           <div className="footer-copyright">
             <div className="container">
             © {new Date().getFullYear()} Copyright Text
-            <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+            <a className="grey-text text-lighten-4 right" href="https://github.com/aldente0/react-shop">Repo</a>
             </div>
           </div>
         </footer>
--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -4,7 +4,7 @@ export function Headers() {
             <div className="nav-wrapper">
                 <a href="!#" className="brand-logo">React Shop</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="!#">Repo</a></li>
+                    <li><a href="https://github.com/aldente0/react-shop">Repo</a></li>
                 </ul>
             </div>
         </nav>
